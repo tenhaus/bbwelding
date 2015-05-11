41d97d1d36ddfee58ab7a4931d34d3ed76c5ec1e
Chris Hayen <chayen@gmail.com>
2015-05-11 15:41:06 -0400

    First commit

---
+++ keystone.js
@@ -0,0 +1,72 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+
+	'name': 'B&B Welding',
+	'brand': 'B&B Welding',
+	
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'jade',
+	
+	'emails': 'templates/emails',
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': '*x"eu}fO=F"=,~<NP"o1XT~,.beXo|`dfERZynC8x]#N,.SzQ;H3--0aZ=sMT)zq'
+
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
+});
+
+keystone.set('email rules', [{
+	find: '/images/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+}, {
+	find: '/keystone/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+}]);
+
+keystone.set('email tests', require('./routes/emails'));
+
+keystone.set('nav', {
+	'posts': ['posts', 'post-categories'],
+	'galleries': 'galleries',
+	'enquiries': 'enquiries',
+	'users': 'users'
+});
+
+keystone.start();
