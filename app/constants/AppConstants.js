50c152c08788d97cecc73be555a5aff7c41f3b8e
Chris Hayen <chayen@gmail.com>
2015-07-16 19:12:25 -0400

    Updated title

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -3,5 +3,5 @@
 import pkg from '../../package';
 
 export const DEBUG = (process.env.NODE_ENV !== 'production');
-export const APP_TITLE = pkg.name;
+export const APP_TITLE = 'B&B Welding';
 export const NAVIGATION_UPDATED = 'NAVIGATION_UPDATED';
