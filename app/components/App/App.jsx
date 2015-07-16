1ac5ca4734c82f6a97c70d6478660133cda2745e
Chris Hayen <chayen@gmail.com>
2015-07-16 19:40:14 -0400

    Showing hands image on the work page

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -12,6 +12,18 @@ import RetinaImage from 'react-retina-image';
 import NavigationStore from '../../stores/NavigationStore';
 import Navigation from '../Navigation/Navigation';
 
+import isRetina from 'is-retina';
+
+import HandsImage from './images/hands.jpg';
+import HandsImageRetina from './images/hands@2x.jpg';
+
+var style = {
+  work: {
+    backgroundImage: isRetina()? 'url('+HandsImageRetina+')': 'url('+HandsImage+')' ,
+    backgroundSize: 'cover'
+  }
+};
+
 function getState() {
   return {
     pages: NavigationStore.getAll()
@@ -25,9 +37,27 @@ class App extends React.Component {
     this.state = getState();
   }
 
+  getSelectedPage() {
+    let router = this.context.router;
+    let pages = this.state.pages;
+    for (let i = pages.length - 1; i >= 0; i--) {
+      let page = pages[i];
+      let isPageActive = Boolean(page.route && router.isActive(page.route));
+      if (isPageActive) { return page; }
+    }
+    return -1;
+  }
+
   render() {
+    var pageStyle = {};
+    var selectedPage = this.getSelectedPage();
+
+    if(selectedPage.route === 'work') {
+      pageStyle = style.work;
+    }
+
     return (
-      <div className={'app'}>
+      <div className='app' style={pageStyle}>
         <div className='header'>
           <RetinaImage src={Logo} />
           <Navigation pages={this.state.pages} />
--- app/components/WorkPage/WorkPage.js
+++ app/components/WorkPage/WorkPage.js
@@ -1,12 +1,30 @@
 'use strict'
 
+import './_WorkPage.scss';
+
 import React from 'react';
+// import isRetina from 'is-retina';
+//
+// import HandsImage from './images/hands.jpg';
+// import HandsImageRetina from './images/hands@2x.jpg';
+//
+// var style = {
+//   base: {
+//     backgroundImage: isRetina()? 'url('+HandsImageRetina+')': 'url('+HandsImage+')' ,
+//     backgroundSize: 'cover'
+//   }
+// };
 
 class WorkPage extends React.Component {
   render() {
     return (
-      <div id="work-page">
-        Our Work
+      <div className="work-page">
+        <div className='content-wrapper'>
+          <div className='content'>
+            Work Page
+          </div>
+        </div>
+
       </div>
     );
   }
