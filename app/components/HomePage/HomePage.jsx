7470f9b9a8b966c05bef2b4ae16ed81b4b939227
Chris Hayen <chayen@gmail.com>
2015-07-16 19:01:40 -0400

    Desktop and hd homepage

--- app/components/HomePage/HomePage.jsx
+++ app/components/HomePage/HomePage.jsx
@@ -1,12 +1,24 @@
 'use strict';
 
+import './_HomePage.scss';
+
 import React from 'react';
+import RetinaImage from 'react-retina-image';
+
+import WelderImage from './images/welder.jpg';
+import WelderImageRetina from './images/welder@2x.jpg';
 
 class HomePage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='home-page'>
+        <RetinaImage className='welder-image' src={WelderImage} />
+        <div className='copy'>
+          <h1>The smartest working shop in the steel business</h1>
+          <p>ONE &quot;B&quot; FOR BRAINS; ONE &quot;B&quot; FOR BRAWN.</p>
+        </div>
+
       </div>
     );
   }
