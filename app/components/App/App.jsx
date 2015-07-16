e90ae9f2555f5ecce5f23322bf5d1dcc777ea8b2
Chris Hayen <chayen@gmail.com>
2015-07-16 18:07:03 -0400

    Added header

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -2,8 +2,13 @@
 
 import './_App.scss';
 
+import Logo from './images/logo.png';
+import LogoRetina from './images/logo@2x.png';
+
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import RetinaImage from 'react-retina-image';
+
 import NavigationStore from '../../stores/NavigationStore';
 import Navigation from '../Navigation/Navigation';
 
@@ -23,7 +28,11 @@ class App extends React.Component {
   render() {
     return (
       <div className={'app'}>
-        <Navigation pages={this.state.pages} />
+        <div className='header'>
+          <RetinaImage src={Logo} />
+          <Navigation pages={this.state.pages} />
+        </div>
+
         <RouteHandler key={this.context.router.getCurrentPath()} />
       </div>
     );
--- app/components/HomePage/HomePage.jsx
+++ app/components/HomePage/HomePage.jsx
@@ -6,9 +6,7 @@ class HomePage extends React.Component {
 
   render() {
     return (
-      <div className={'page__home'}>
-        <h1>Home</h1>
-        <p>Welcome to the home page.</p>
+      <div>
       </div>
     );
   }
