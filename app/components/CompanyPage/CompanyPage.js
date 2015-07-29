66d4d72b95e5a7e473047beb302eef696a705cb7
Chris Hayen <chayen@gmail.com>
2015-07-29 15:58:58 -0400

    Layed out company page

--- app/components/CompanyPage/CompanyPage.js
+++ app/components/CompanyPage/CompanyPage.js
@@ -1,16 +1,52 @@
-'use strict'
-
 import React from 'react';
+import Radium from 'radium';
+
+import RetinaImage from 'react-retina-image';
+
+import FlameImage from './images/flame.png';
+import FlameImageRetina from './images/flame@2x.png';
+import MachineImage from './images/machine.png';
+import MachineImageRetina from './images/machine@2x.png';
+import AiscImage from './images/aisc.png';
+import AiscImageRetina from './images/aisc@2x.png';
+
+
 import Style from './_CompanyPage.Style';
 
 class CompanyPage extends React.Component {
   render() {
     return (
       <div id="company-page" style={Style.base}>
-        Our Company
+        <div style={Style.contentWrapper}>
+          <div style={Style.content}>
+            <h1 style={Style.title}>Our Company</h1>
+            <div style={Style.topSection}>
+
+              {/* Section */}
+              <div style={Style.halfSection}>
+                <RetinaImage style={Style.image} src={FlameImage} />
+                <p>We took the <em>hardest working</em> people in the steel business and made them <em>smarter</em>.</p>
+              </div>
+
+              {/* Section */}
+              <div style={Style.halfSection}>
+                <RetinaImage style={Style.image} src={MachineImage} />
+                <p>We gave them big, powerful machines and a 35,000 square foot shop.</p>
+              </div>
+            </div>
+
+            {/* Section */}
+            <div style={Style.fullSection}>
+              <RetinaImage style={Style.image} src={AiscImage} />
+              <p>Add on AISC membership, a written quality control program, and the ability to electronically import designs straight from the engineers and what you get is one of the most technology-driven steel fabrication shops in the region.</p>
+              <p>From small jobs to 500-ton jobs, B&B will complete them swifter, more affordably and with better quality.</p>
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default CompanyPage;
+export default Radium(CompanyPage);
--- app/components/CompanyPage/_CompanyPage.Style.js
+++ app/components/CompanyPage/_CompanyPage.Style.js
@@ -1,5 +1,44 @@
+import HandsImage from './images/hands.jpg';
+import HandsImageRetina from './images/hands@2x.jpg';
+import isRetina from 'is-retina';
+
+var backgroundImage = isRetina()? 'url('+HandsImageRetina+')': 'url('+HandsImage+')';
+
 export default {
   base: {
-    color: '#fff'
+    height: '100%',
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover'
+  },
+
+  contentWrapper: {
+    color: '#fff',
+    height: '100%'
+  },
+
+  content: {
+    backgroundColor: 'rgba(0,0,0,.5)',
+    minHeight: '400px',
+    height: '100%',
+    padding: '4rem'
+  },
+
+  title: {
+    fontWeight: 500,
+    marginBottom: '2rem'
+  },
+
+  topSection: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+
+  halfSection: {
+    display: 'inline-block',
+    width: '40%'
+  },
+
+  image: {
+    marginRight: '1rem'
   }
 }
