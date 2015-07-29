29a1da045ac85014a8b6ac33ac3f7093012f60be
Chris Hayen <chayen@gmail.com>
2015-07-29 13:03:49 -0400

    Added company page

--- app/components/CompanyPage/CompanyPage.js
+++ app/components/CompanyPage/CompanyPage.js
@@ -1,11 +1,12 @@
 'use strict'
 
 import React from 'react';
+import Style from './_CompanyPage.Style';
 
 class CompanyPage extends React.Component {
   render() {
     return (
-      <div id="company-page">
+      <div id="company-page" style={Style.base}>
         Our Company
       </div>
     );
