45a84f49805f19afc58c61c38e3c80dc71bc9ab6
Chris Hayen <chayen@gmail.com>
2015-08-02 14:26:30 -0400

    Common page component. Refactored Contact

--- app/components/Page/Page.js
+++ app/components/Page/Page.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Radium from 'radium';
 
-import Style from './Page.Style';
+import Style from './_Page.Style';
 
 class Page extends React.Component {
 
   render() {
     return (
       <div className='page' style={Style.base}>
-        
+        <div id="contact-page" style={Style.base}>
+          <div style={Style.contentWrapper}>
+            <div style={Style.content}>
+              <h1 style={Style.title}>{this.props.title}</h1>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- app/components/Page/_Page.Style.js
+++ app/components/Page/_Page.Style.js
@@ -1,5 +1,21 @@
 export default {
   base: {
+    height: '100%'
+  },
 
+  contentWrapper: {
+    color: '#fff',
+    height: '100%'
+  },
+
+  content: {
+    backgroundColor: 'rgba(0,0,0,.6)',
+    minHeight: '400px',
+    height: '100%',
+    padding: '4rem'
+  },
+
+  title: {
+    fontWeight: 500
   }
 };
