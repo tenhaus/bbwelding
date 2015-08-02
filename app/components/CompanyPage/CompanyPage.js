3d0900dec880d5fd386e8854129222bd0c25b677
Chris Hayen <chayen@gmail.com>
2015-08-02 14:37:48 -0400

    Refactored company page

--- app/components/CompanyPage/CompanyPage.js
+++ app/components/CompanyPage/CompanyPage.js
@@ -11,49 +11,44 @@ import MachineImageRetina from './images/machine@2x.png';
 import AiscImage from './images/aisc.png';
 import AiscImageRetina from './images/aisc@2x.png';
 
-
+import Page from '../Page/Page';
 import Style from './_CompanyPage.Style';
 
 class CompanyPage extends React.Component {
   render() {
     return (
-      <div id="company-page" style={Style.base}>
-        <div style={Style.contentWrapper}>
-          <div style={Style.content}>
-            <h1 style={Style.title}>Our Company</h1>
+      <Page title='Our Company'>
 
-            <div style={Style.topSection}>
+        <div style={Style.topSection}>
 
-              {/* Section */}
-              <div style={Style.halfSection} key='left'>
-                <div style={Style.halfDetail}>
-                  <RetinaImage style={Style.image} src={FlameImage} />
-                  <p style={Style.flameText}>We took the <em>hardest working</em> people in the steel business and made them <em>smarter</em>.</p>
-                </div>
-                <div style={Style.halfDetail}>
-                  <RetinaImage style={Style.image} src={MachineImage} />
-                  <p style={Style.machineText}>We gave them big, <em>powerful machines</em> and a 35,000 square foot shop.</p>
-                </div>
-              </div>
-
-              {/* Section */}
-              <div style={Style.halfSection} key='right'>
-                <iframe src="https://player.vimeo.com/video/134711747"
-                  width="100%" height="240px" frameborder="0"
-                  webkitallowfullscreen mozallowfullscreen allowfullscreen />
-              </div>
+          {/* Section */}
+          <div style={Style.halfSection} key='left'>
+            <div style={Style.halfDetail}>
+              <RetinaImage style={Style.image} src={FlameImage} />
+              <p style={Style.flameText}>We took the <em>hardest working</em> people in the steel business and made them <em>smarter</em>.</p>
             </div>
-
-            {/* Section */}
-            <div style={Style.fullSection}>
-              <RetinaImage style={Style.image} src={AiscImage} />
-              <p>Add on AISC membership, a written quality control program, and the ability to electronically import designs straight from the engineers and what you get is one of the most <em>technology-driven steel fabrication</em> shops in the region.</p>
-              <p>From small jobs to 500-ton jobs, B&B will complete them swifter, more affordably and with better quality.</p>
+            <div style={Style.halfDetail}>
+              <RetinaImage style={Style.image} src={MachineImage} />
+              <p style={Style.machineText}>We gave them big, <em>powerful machines</em> and a 35,000 square foot shop.</p>
             </div>
+          </div>
 
+          {/* Section */}
+          <div style={Style.halfSection} key='right'>
+            <iframe src="https://player.vimeo.com/video/134711747"
+              width="100%" height="240px" frameborder="0"
+              webkitallowfullscreen mozallowfullscreen allowfullscreen />
           </div>
         </div>
-      </div>
+
+        {/* Section */}
+        <div style={Style.fullSection}>
+          <RetinaImage style={Style.image} src={AiscImage} />
+          <p>Add on AISC membership, a written quality control program, and the ability to electronically import designs straight from the engineers and what you get is one of the most <em>technology-driven steel fabrication</em> shops in the region.</p>
+          <p>From small jobs to 500-ton jobs, B&B will complete them swifter, more affordably and with better quality.</p>
+        </div>
+        
+      </Page>
     );
   }
 }
--- app/components/CompanyPage/_CompanyPage.Style.js
+++ app/components/CompanyPage/_CompanyPage.Style.js
@@ -1,25 +1,4 @@
 export default {
-  base: {
-    height: '100%',
-    fontWeight: '300'
-  },
-
-  contentWrapper: {
-    color: '#fff',
-    height: '100%'
-  },
-
-  content: {
-    backgroundColor: 'rgba(0,0,0,.6)',
-    minHeight: '400px',
-    height: '100%',
-    padding: '4rem'
-  },
-
-  title: {
-    fontWeight: 500,
-    marginBottom: '2rem'
-  },
 
   topSection: {
     display: 'flex',
