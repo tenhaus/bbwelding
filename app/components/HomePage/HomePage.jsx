3448ef79cac62c8e2e32ee8cef5c713ba2d6fbcb
Chris Hayen <chayen@gmail.com>
2015-11-20 14:06:14 -0500

    added new landing image

--- app/components/HomePage/HomePage.jsx
+++ app/components/HomePage/HomePage.jsx
@@ -5,15 +5,15 @@ import './_HomePage.scss';
 import React from 'react';
 import RetinaImage from 'react-retina-image';
 
-import WelderImage from './images/welder.jpg';
-import WelderImageRetina from './images/welder@2x.jpg';
+import AbstractImage from './images/abstract.png';
+import AbstractImageRetina from './images/abstract@2x.png';
 
 class HomePage extends React.Component {
 
   render() {
     return (
       <div className='home-page'>
-        <RetinaImage className='welder-image' src={WelderImage} />
+        <RetinaImage className='welder-image' src={AbstractImage} />
         <div className='copy'>
           <h1>The smartest working shop in the steel business</h1>
           <p>ONE &quot;B&quot; FOR BRAINS; ONE &quot;B&quot; FOR BRAWN.</p>
